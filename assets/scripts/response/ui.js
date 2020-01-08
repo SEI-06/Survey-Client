6b6167eaea534b3bba56247a0065c18dce73dbd8
wyang19a <wyang19a@gmail.com>
2020-01-08 16:12:07 -0500

    Fixed bug with result view page
    added if statement to skip responses with empty question object.

--- assets/scripts/response/ui.js
+++ assets/scripts/response/ui.js
@@ -10,12 +10,14 @@ const onindexResultsSuccess = (data) => {
   const choiceTitle = []
   // console.log('D', data.responses[5].questionOwner._id)
   for (let i = 0; i < data.responses.length; i++) {
-    if (questionId === data.responses[i].questionOwner._id) {
-      answerChoices.push(data.responses[i].choice)
-      choiceTitle.push(data.responses[i].questionOwner.choice1)
-      choiceTitle.push(data.responses[i].questionOwner.choice2)
-      choiceTitle.push(data.responses[i].questionOwner.choice3)
-      choiceTitle.push(data.responses[i].questionOwner.choice4)
+    if (data.responses[i].questionOwner) {
+      if (questionId === data.responses[i].questionOwner._id) {
+        answerChoices.push(data.responses[i].choice)
+        choiceTitle.push(data.responses[i].questionOwner.choice1)
+        choiceTitle.push(data.responses[i].questionOwner.choice2)
+        choiceTitle.push(data.responses[i].questionOwner.choice3)
+        choiceTitle.push(data.responses[i].questionOwner.choice4)
+      }
     }
   }
   for (let i = 0; i < answerChoices.length; i++) {
